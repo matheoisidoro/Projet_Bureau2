73b67b93a5e74978ccd35b8615c4ef50073f2e92
isidoro <isidoro.matheo@gmail.com>
2024-09-21 17:34:07 +0200

    Style, Police, Menu

--- Test fonctionaliter/icone_menu.js
+++ Test fonctionaliter/icone_menu.js
@@ -4,12 +4,17 @@ const menuModal = document.getElementById('menuModal');
 
 // Ouvrir le menu lorsqu'on clique sur l'icône
 menuIcon.addEventListener('click', function() {
-  menuModal.classList.add('active');
+    menuModal.classList.add('active');
 });
 
-// Fermer le menu lorsqu'on clique à l'extérieur du contenu
-menuModal.addEventListener('click', function(event) {
-  if (event.target === menuModal) {
-    menuModal.classList.remove('active');
-  }
+// Fermer le menu lorsqu'on clique à l'extérieur du contenu du menu
+document.addEventListener('click', function(event) {
+    // Vérifier si le clic est à l'extérieur de la modale et de l'icône du menu
+    const isClickInsideMenu = menuModal.contains(event.target);
+    const isClickOnIcon = menuIcon.contains(event.target);
+
+    if (!isClickInsideMenu && !isClickOnIcon) {
+        // Si le clic est en dehors du menu et de l'icône, on ferme le menu
+        menuModal.classList.remove('active');
+    }
 });
