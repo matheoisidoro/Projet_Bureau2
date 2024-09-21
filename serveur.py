63f7240b9ef25e9ae19d98c4065524dfc4bd0155
isidoro <isidoro.matheo@gmail.com>
2024-09-21 19:08:49 +0200

    serveur

--- serveur.py
+++ serveur.py
@@ -2,7 +2,7 @@ import http.server
 import socketserver
 
 PORT = 8080
-HOST = "192.168.1.35"  # Permet à tous les périphériques sur le réseau local d'accéder au serveur
+HOST = "localhost"  # Permet à tous les périphériques sur le réseau local d'accéder au serveur
 
 Handler = http.server.SimpleHTTPRequestHandler
 
